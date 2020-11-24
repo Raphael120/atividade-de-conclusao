a21a0b24e401c3729cf2739f3af0336e57737956
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2020-11-23 22:03:24 -0300

    atividade de conclusão

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,11 +4,11 @@ class Home extends Controller {
     public function index($param1= '', $param2= '', $param3= '') {
         $this->view('home/index');
     } 
-    public function form(){
-        echo "esse e seu formulario";
-    }
-    public function cadastro(){
-        echo "voce foi cadastrado";
-    }
+    // public function form(){
+    //     echo "esse e seu formulario";
+    // }
+    // public function cadastro(){
+    //     echo "voce foi cadastrado";
+    // }
 }
 ?>
